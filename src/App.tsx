5c0b72ce3809812cf9ae4131ea4dcac221a97ff6
dxmlk <1101sky@korea.ac.kr>
2026-08-11 20:02:40 +0900

    안드로이드 기기 접속 시 플레이스토어로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import PCPage from "./components/tempPages/PCPage";
 
 const APP_STORE_URL =
   "https://apps.apple.com/kr/app/%EC%83%88%EB%A1%9D-%EC%9D%BC%EC%83%81-%EC%86%8D%EC%9D%98-%ED%83%90%EC%A1%B0-%EC%9D%BC%EC%A7%80/id6744866662";
-const ANDROID_PRE_REGISTRATION_URL =
-  "https://docs.google.com/forms/d/e/1FAIpQLSc_LJXcdnmmNC4smdBlBnR_G8z9XvhY_1oyXEkVyyoSFOLF5Q/viewform?usp=dialog";
+const PLAY_STORE_URL =
+  "https://play.google.com/store/apps/details?id=com.apu.saerok";
 
 function App() {
   useEffect(() => {
@@ -24,7 +24,7 @@ function App() {
     }
 
     if (isAndroid) {
-      window.location.href = ANDROID_PRE_REGISTRATION_URL;
+      window.location.href = PLAY_STORE_URL;
     }
   }, []);
 
